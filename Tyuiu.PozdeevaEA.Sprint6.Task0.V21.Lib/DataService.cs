0b8e3ec017b68c3e18c6c5eea5aa62095f98494e
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-11-30 02:23:22 +0500

    Task0

--- Tyuiu.PozdeevaEA.Sprint6.Task0.V21.Lib/DataService.cs
+++ Tyuiu.PozdeevaEA.Sprint6.Task0.V21.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.PozdeevaEA.Sprint6.Task0.V21.Lib
         public double Calculate(int x)
         {
             double res = Math.Round(2 * Math.Pow(x, 3) + 0.5 * Math.Pow(x, 2) - 3.5 * x + 2,3);
-            return res;
+            return res; 
         }
     }
 }
